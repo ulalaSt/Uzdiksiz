5c11bffcb9dfc45beaa0dcf4bf2454b3f759df20
Ulan <ulalaseit@gmail.com>
2025-03-21 08:09:22 +0500

    Initial Commit

---
+++ Uzdiksiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uzdiksiz
+//
+//  Created by Ulan Seitkali on 21.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uzdiksiz/UzdiksizApp.swift
@@ -0,0 +1,17 @@
+//
+//  UzdiksizApp.swift
+//  Uzdiksiz
+//
+//  Created by Ulan Seitkali on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct UzdiksizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
